6144a85567bb4c9e36c0b2f53252f168381e0f3d
Shahed Amer <chanyeol6521354@gmail.com>
2023-02-03 12:37:45 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Maak je eigen pizza</h1>
+    <form>
+        <label for="pizza">Bodemformaat</label>
+        <select id="pizza" name="pizza">
+            <option value="20">Maak je keuze</option>
+            <option value="20">20 cm</option>
+            <option value="25">25 cm</option>
+            <option value="30">30 cm</option>
+            <option value="40">40 cm</option>
+        </select>
+        <br><br>
+        <label for="saus">Saus</label>
+        <select id="saus" name="saus">
+            <option value="20">Maak je keuze</option>
+            <option value="20">Tomaatensaus</option>
+            <option value="25">Extra Tomaatensaus</option>
+            <option value="30">Spicy Tomaatensaus</option>
+            <option value="40">BBQ saus</option>
+            <option value="40">Creme fraiche</option>
+        </select>
+        <br>
+        <label for="">Pizzatoppings</label>
+        <br>
+        <input type="radio" id="vegan" name="" value="HTML">
+        <label for="html">Vegan</label><br>
+        <input type="radio" id="vegatarsich" name="" value="CSS">
+        <label for="css">vegatarisch</label><br>
+        <input type="radio" id="vlees" name="" value="JavaScript">
+        <label for="javascript">vlees</label> <br><br>
+
+        <label for="">Kruiden</label>
+        <br>
+        <input type="checkbox" id="" name="" value="">
+        <label for="">Peterselie</label><br>
+        <input type="checkbox" id="" name="" value="">
+        <label for="">Oregano</label><br>
+        <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+        <label for="">Chili Flakes</label> <br>
+        <input type="checkbox" name="" value="">
+        <label for="">Zwarte peper</label>
+        <br>
+        <button for="submit">Bestel</button>
+        <br>
+    </form>
+</body>
+
+</html>
